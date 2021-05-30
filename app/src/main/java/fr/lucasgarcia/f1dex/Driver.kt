dcc9116b49cfcadc257349b68d9b3ffd9c6c6f3f
Lucas <lucasgarcia@hotmail.fr>
2021-05-30 02:05:09 +0200

    modification attributs drivers

--- app/src/main/java/fr/lucasgarcia/f1dex/Driver.kt
+++ app/src/main/java/fr/lucasgarcia/f1dex/Driver.kt
@@ -3,7 +3,9 @@ package fr.lucasgarcia.f1dex
 import java.util.*
 
 data class Driver(
+    val id: Int,
     val firstName: String,
     val lastName: String,
-    val birthDate: Date
+    val birthDate: Date,
+    val nationality: String
     )
